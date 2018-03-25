26a03bfe1048ed29ab4cc464240bdd60a61f235a
vagrant <vagrant@dev-node.co.za>
2018-03-25 23:02:50 +0000

    tests

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from snippets.models import Snippet
+
+class SnippetTestCase(TestCase):
+    def setUp(self):
+        Snippet.objects.create(title="test", code="test", linenos=False, language="python", style="friendly")
+
+    def test_can_get_snippet(self):
+        snippet = Snippet.objects.get(title="test")
+        self.assertEqual(snippet.language, 'python')
